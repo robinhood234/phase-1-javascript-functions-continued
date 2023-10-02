2a6a8b35c8c888b1a944c6981c5e9d234c8db846
robin nganga smith <saintofcookies.2021@gmail.com>
2023-10-02 19:52:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bathe my dog');
+function mondayWork(activity2 = 'go to the office') {
+return `This Monday, I will ${activity2}.`
+}
+mondayWork('work from home');
+function wrapAdjective () {
+  const parametre = wrapAdjective("*");
+  return function (adjective = 'special') {
+    
+  }
+}
